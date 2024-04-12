b99c48597c3aaf073922ab575260be13ad049f7c
stephenlambertfs <stephenolambert@gmail.com>
2024-04-12 10:57:53 -0600

    added solution

--- server/app.py
+++ server/app.py
@@ -46,6 +46,25 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        data = request.get_json()
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 202)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return "", 204
+
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
